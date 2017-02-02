055b515ca12732030bca736cef00713d31911798
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-02-02 14:27:52 -0800

    Add turns for each player, temp score will add to each players score when end turn is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,23 @@
 function Player(name, score) {
   this.name = name;
   this.score = 0;
-  this.turn = true;
+}
+
+Player.prototype.addToScore = function() {
+  this.score += rollDie();
 }
 
 function rollDie() {
-  return Math.floor(Math.random() * 6) + 1;
+  return Math.floor(Math.random() * 6) + 2;
 }
+
+var turn = true;
+
+function switchTurn() {
+  turn = !turn;
+}
+
+var tempScore = 0;
 
 //*****FRONT-END*****
 
@@ -34,6 +45,40 @@ $(function() {
     $("#player_2_info").append("<h3>" + playerTwo.name + "</h3><br><h3>" + playerOne.score + "</h3>");
     $("#buttons").append("<br><button type=\"button\" id=\"roll\" class=\"btn\">Roll!</button> <button type=\"button\" id=\"end_turn\" class=\"btn\">End Turn</button>");
 
+    $("#roll").click(function() {
+      if (turn) {
+        rollDie();
+        $("#temp_roll_score").empty().append("<h3>Current total: " + tempScore + "!</h3>");
+        if (rollDie() === 1) {
+          alert("You got a one, and thus, you lose!");
+          switchTurn();
+        } else {
+          tempScore += rollDie();
+        }
+      } else if (!turn) {
+        rollDie();
+        $("#temp_roll_score").empty().append("<h3>Current total: " + tempScore + "!</h3>");
+        if (rollDie() === 1) {
+          alert("You got a one, and thus, you lose!");
+          switchTurn();
+        } else {
+          tempScore += rollDie();
+        }
+      }
+    });
 
+    $("#end_turn").click(function() {
+      if (turn) {
+        playerOne.score += tempScore;
+        $("#player_1_info").empty().append("<h3>" + playerOne.name + "</h3><br><h3>" + playerOne.score + "</h3>");
+        tempScore = 0;
+        switchTurn();
+      } else if (!turn) {
+        playerTwo.score += tempScore;
+        $("#player_2_info").empty().append("<h3>" + playerTwo.name + "</h3><br><h3>" + playerTwo.score + "</h3>");
+        tempScore = 0;
+        switchTurn();
+      }
+    });
   });
 });
